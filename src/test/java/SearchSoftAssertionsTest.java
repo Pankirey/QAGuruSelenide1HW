067c7131b59be15aaab5eab1ec5d2a2d2c0edbfc
Pankirey <pankirey@gmail.com>
2024-03-21 19:14:40 +0500

    first commit

---
+++ src/test/java/SearchSoftAssertionsTest.java
@@ -0,0 +1,37 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.ByText;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchSoftAssertionsTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void successfulSearchSoftAssertionsTest() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $x("//a[text()='Soft assertions']").shouldBe(visible);
+        $x("//a[text()='Soft assertions']").click();
+        $$("pre").findBy(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}")).shouldBe(visible);
+    }
+
+}
